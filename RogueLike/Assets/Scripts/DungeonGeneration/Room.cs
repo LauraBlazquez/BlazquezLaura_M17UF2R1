ac997cff3800300c59f039a42e18de4e7e3b95e8
Laura Blazquez <laura.blazquez.7e7@itb.cat>
2025-01-06 02:58:24 +0100

    Finalizacion de generacion procedural de salas, principio de puertas, añadido de sprites nuevos y editado de viejos, desarrollo de spawns de enemigos

--- RogueLike/Assets/Scripts/DungeonGeneration/Room.cs
+++ RogueLike/Assets/Scripts/DungeonGeneration/Room.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public int Width;
-    public int Height;
-    public int X;
-    public int Y;
-    // Start is called before the first frame update
+    public int Width, Height, X, Y;
+    public Door up, left, down, right;
+    public List<Door> doors = new List<Door>();
+
     void Start()
     {
         if(RoomController.instance == null)
@@ -16,15 +15,80 @@ public class Room : MonoBehaviour
             Debug.Log("Pressed play in the wrong scene!");
             return;
         }
+        Door[] ds = GetComponentsInChildren<Door>();
+        foreach(Door d in ds)
+        {
+            doors.Add(d);
+            switch (d.doorType)
+            {
+                case Door.DoorType.up:
+                    up = d; 
+                break;
+                case Door.DoorType.left:
+                    left = d;
+                break;
+                case Door.DoorType.down:
+                    down = d;
+                break;
+                case Door.DoorType.right:
+                    right = d;
+                break;
+            }
+        }
         RoomController.instance.RegisterRoom(this);
     }
-    private void OnDrawGizmos()
+
+    public void RemoveUnconnectedDoors()
+    {
+        foreach (Door door in doors)
+        {
+            int posX, posY;
+            posX = posY = 0;
+            switch (door.doorType)
+            {
+                case Door.DoorType.up:
+                    posY = 1;
+                    break;
+                case Door.DoorType.left:
+                    posX = -1;
+                    break;
+                case Door.DoorType.down:
+                    posY = -1;
+                    break;
+                case Door.DoorType.right:
+                    posX = 1;
+                    break;
+            }
+            if (GetRoomByDoor(posX, posY) == null)
+                door.gameObject.SetActive(false);
+        }
+    }
+
+    public Room GetRoomByDoor(int posX, int posY)
+    {
+        if (RoomController.instance.DoesRoomExist(X + posX, Y + posY))
+        {
+            return RoomController.instance.FindRoom(X + posX, Y + posY);
+        }
+        return null;
+    }
+
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, new Vector3(Width,Height, 0));
     }
+
     public Vector3 GetRoomCentre()
     {
         return new Vector3(X * Width, Y * Height);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RoomController.instance.OnPlayerEnterRoom(this);
+        }
     }
 }
