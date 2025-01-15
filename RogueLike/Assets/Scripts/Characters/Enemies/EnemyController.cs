22664d14b6bf660ad2429c09fe9eb1b9a6005fa7
Javier Fernández Cano <javier.fernandez.7e7@itb.cat>
2025-01-15 18:19:50 +0100

    comienzo de FSM

--- RogueLike/Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ RogueLike/Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public int HP;
+    public GameObject target;
+    //private ChaseBehaviour chaseB;
     public Animator animator;
 
     private void Start()
@@ -11,16 +14,21 @@ public class EnemyController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
-    { 
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             animator.SetTrigger("Spawned");
+        }
+    }
+
+    public void GoToState<T>() where T : StatesSO
+    {
+        if (CurrentState.StatesToGo.Find(StatesSO => StatesSO is T))
+        {
+            CurrentState.OnStateExit(this);
+            CurrentState = CurrentState.StatesToGo.Find(obj => obj is T);
+            CurrentState.OnStateEnter(this);
         }
     }
 }
---
+++ RogueLike/Assets/Scripts/Characters/Enemies/FSM/PreSpawnState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="PreSpawnState", menuName ="StatesSO/PreSpawn")]
+
+public class PreSpawnState : StatesSO
+{
+    public override void OnStateEnter(EnemyController ec)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStateExit(EnemyController ec)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStateUpdate(EnemyController ec)
+    {
+        throw new System.NotImplementedException();
+    }
+}
